eb903209977eff41269f5d7bdcf39249090165d8
Y.V <straysheep753@gmail.com>
2025-07-16 12:51:36 -0600

    3. Menú funcional, ahora podrá terminar el programa.

--- Actividad_3_git_test.py
+++ Actividad_3_git_test.py
@@ -1,21 +1,27 @@
 key = True
+counter = 0
+use = False
 while key:
-    for attempt in range(4):
-        user = input("Coloque su username: ")
-        password = input("Coloque su contraseña: ")
-        if user == "nosoyadmin" and password == "4563":
-            beam = True
-            while beam:
-                print("\n\n-------------Menu--------------\n1. Calcular temperatura\n2. Calcular el área de un triángulo\n3. Salir")
-                ops_man = input("Opción: ")
-                match ops_man:
-                    case "1":
-                        pass
-                    case "2":
-                        pass
-                    case "3":
-                        beam = False
+    if use:
         key = False
-    print("\n\nAcceso rechazado, vuelva más tarde")
-    key = False
+    counter +=1
+    user = input("Coloque su username: ")
+    password = input("Coloque su contraseña: ")
+    if user == "nosoyadmin" and password == "4563":
+        key = False
+        beam = True
+        while beam:
+            print("\n\n-------------Menu--------------\n1. Calcular temperatura\n2. Calcular el área de un triángulo\n3. Salir")
+            ops_man = input("Opción: ")
+            match ops_man:
+                case "1":
+                    pass
+                case "2":
+                    pass
+                case "3":
+                    use = True
+                    beam = False
+    elif counter == 4:
+        print("\n\nAcceso rechazado, vuelva más tarde")
+        key = False
 print("Gracias por usar el programa.")
