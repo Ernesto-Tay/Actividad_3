dd7fdd5cd6e56ac87d0322d3a3fe7a9219ad054f
jorgeernestotayruiz <tayocruz1772@gmailcom>
2025-07-16 15:04:54 -0600

    5. Funcionamiento de la opción 1 con adición de filtros anti-crashing

--- Actividad_3_git_test.py
+++ Actividad_3_git_test.py
@@ -15,7 +15,28 @@ while key:
             ops_man = input("Opción: ")
             match ops_man:
                 case "1":
-                    pass
+                    temp_ops = int(input("\n"+"-"*5+"Conversor de temperaturas"+"-"*5+"\n1. Celsius a Farenheit\n2. Farenheit a Celsius\nOpción a elegir:"))
+                    if temp_ops == 1:
+                        while True:
+                            try:
+                                c_temp = int(input("Ingrese la temperatura en Celsius: "))
+                                break
+                            except:
+                                print("Valor inválido. Ingresar solo números\n")
+                        f_end_temp = (c_temp*1.8)+32
+                        print(f"La temperatura en Farenheit es de {f_end_temp}°\n")
+
+                    elif temp_ops == 2:
+                        while True:
+                            try:
+                                f_temp = int(input("Ingrese la temperatura en Farenheit: "))
+                                break
+                            except:
+                                print("Valor inválido, ingresar solo números\n")
+                        c_end_temp = (f_temp - 32)/1.8
+                        print(f"La temperatura en Celsius es de {c_end_temp}°\n")
+
+
                 case "2":
                     pass
                 case "3":
