7a5c632158b843e949a19a8151ec25000dd30951
Y.V <straysheep753@gmail.com>
2025-07-16 12:07:15 -0600

    2. Menú creado, hace falta hacer que el programa termine

--- Actividad_3_git_test.py
+++ Actividad_3_git_test.py
@@ -1,1 +1,21 @@
-print("-------------Menu--------------")
+key = True
+while key:
+    for attempt in range(4):
+        user = input("Coloque su username: ")
+        password = input("Coloque su contraseña: ")
+        if user == "nosoyadmin" and password == "4563":
+            beam = True
+            while beam:
+                print("\n\n-------------Menu--------------\n1. Calcular temperatura\n2. Calcular el área de un triángulo\n3. Salir")
+                ops_man = input("Opción: ")
+                match ops_man:
+                    case "1":
+                        pass
+                    case "2":
+                        pass
+                    case "3":
+                        beam = False
+        key = False
+    print("\n\nAcceso rechazado, vuelva más tarde")
+    key = False
+print("Gracias por usar el programa.")
